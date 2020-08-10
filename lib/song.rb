477ca128f62f9b74a428e56b59fd8030ba4fbe0c
Christie Yoon <cleex1999@gmail.com>
2020-08-10 00:38:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,19 +41,22 @@ class Song
      self.all.sort_by {|x| x.name}
   end
 
-  def self.new_from_filename(filename)
-
-    # binding.pry
-  end
-
-  def self.create_from_filename
+  def self.new_from_filename(file_name)
       file = file_name.split(" - ")
+      new_song = new_by_name(file[1].chomp(".mp3"))
+      new_song.artist_name = file[0]
+      new_song
+    end
 
-  end
+    def self.create_from_filename(file_name)
+      file = file_name.split(" - ")
+      new_song = create_by_name(file[1].chomp(".mp3"))
+      new_song.artist_name = file[0]
+      new_song
+    end
 
   def self.destroy_all
-    @@all.clear
-    @@all = []
+     self.all.clear
   end
 
   def self.all
